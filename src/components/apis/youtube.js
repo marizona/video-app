d71c5284c1b8f05d51d7987a2ea06eb4d0ed000f
marizona <marie.jacquier@epitech.eu>
2022-04-27 16:10:21 +0200

    install axios and config

--- src/components/apis/youtube.js
+++ src/components/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from "axios";
+
 const KEY = "AIzaSyBpM6aA9VmYgjG1DF3IDDvvDsyH1VBv35k";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY,
+  },
+});
